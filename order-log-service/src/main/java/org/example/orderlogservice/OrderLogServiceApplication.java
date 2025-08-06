0476970aa42a86453768c96afe59b0e9f36d25fa
wuya <2628381020@qq.com>
2025-08-06 14:50:58 +0800

    feature: create notification-service module and order-log-service module

---
+++ order-log-service/src/main/java/org/example/orderlogservice/OrderLogServiceApplication.java
@@ -0,0 +1,13 @@
+package org.example.orderlogservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderLogServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderLogServiceApplication.class, args);
+    }
+
+}
